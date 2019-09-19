e5203b7000b3726a663faccd0a7ee77a2c348de2
Tyler Greene <tjgreene@ksu.edu>
2019-09-19 18:55:39 -0500

    fixing counter

--- count_files_and_subdirs.sh
+++ count_files_and_subdirs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #displays number of files and subdirectories in this directory
-echo "There are "
-ls -1 | wc -l
+echo "There are " 
+ls -p | grep -v '/$' | wc -l
 echo "files in this directory"
+
 echo " "
 echo "There are "
-ls -l | grep "^d" | wc -l
+ls -lR | grep "^d" | wc -l
 echo "subdirectories in this directory"
 
 
